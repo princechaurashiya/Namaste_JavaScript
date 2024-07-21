833baef076a9915605ead321f519a1bec6f7970b
princechaurashiya <princechaurashiya163@gmail.com>
2024-07-21 10:06:53 +0530

    first code of js

---
+++ js_04.js
@@ -0,0 +1,22 @@
+
+
+var x = 7 ;
+
+
+// var getName3 = function (){}
+
+
+
+// var getName2 = () => {
+//     console.log("NamastejavaScript");
+// }
+
+ function getName(){
+    console.log("Namaste Js")
+}
+
+getName ();
+console.log(x);
+//console.log(getName);
+
+

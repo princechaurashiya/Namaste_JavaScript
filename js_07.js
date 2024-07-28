4f9964f712c2f2e6c104836ec8f2ff89b371a86b
princechaurashiya <princechaurashiya163@gmail.com>
2024-07-28 07:10:26 +0530

    understanding undefined

---
+++ js_07.js
@@ -0,0 +1,21 @@
+
+var a ;
+console.log(a);
+
+
+
+if (a === undefined){
+    console.log("a is undefined");
+}
+else{
+    console.log("a is not undefined");
+}
+
+
+a = 10;
+console.log(a);
+a = "hello world";
+console.log(a);
+
+
+a = undefined // it is not good practice so we dont have to use like this.
